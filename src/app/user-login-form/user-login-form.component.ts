82e62d822d227b9f7279e12097bc32050529ec51
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-04-23 13:53:47 -0400

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.css']
+  // styleUrls: ['./user-login-form.component.scss']
 })
-export class UserLoginFormComponent {
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' }
 
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  /**
+   * Send the form inputs to the backend via API, store user data on localStorage 
+   * and navigate to main page(/movies)
+   * {@link FetchApiDataService}
+   * @function loginUser
+   */
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe({
+      next: (result) => {
+        // console.log(result);
+        // Store username and token on localStorage
+        localStorage.setItem('user', result.user.Username);
+        localStorage.setItem('token', result.token);
+        this.dialogRef.close(); // Close the modal on success
+        this.snackBar.open(`Welcome Back ${this.userData.Username}`, 'OK', { duration: 2000 });
+        this.router.navigate(['movies']);
+      },
+      error: (error) => {
+        console.log(error);
+        this.snackBar.open(error, 'OK', { duration: 2000 })
+      }
+    })
+  }
 }
