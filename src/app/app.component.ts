afc1895948ace00a87417dd7ecd12fed4fa5b176
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-04-24 11:07:53 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
@@ -22,6 +24,11 @@ export class AppComponent {
     this.dialog.open(UserLoginFormComponent, {
       // Assigning the dialog a width
       width: '280px'
+    });
+  }
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
     });
   }
 }
