25c50543ba57351ab2d0943f48c760563f86a12e
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-04-24 16:31:44 -0400

    Update navigation.component.ts

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  constructor(
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
 
+  ngOnInit(): void { }
+
+  /**
+   * Log out user (clear user data from localStorage) and navigate to welcome page
+   * @function logOutUser
+   */
+  logOutUser(): void {
+    const user = localStorage.getItem('user');
+    this.snackBar.open(`Goodbye ${user}`, 'OK', {
+      duration: 3000
+    });
+    this.router.navigate(['welcome']);
+    localStorage.clear();
+  }
 }
